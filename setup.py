48f8835b55a44969d412105589911cd68cd11470
Praneeth <praneethbedapudi@gmail.com>
2020-11-21 23:53:59 +0530

    update version
    Signed-off-by: Praneeth <praneethbedapudi@gmail.com>

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = "https://github.com/notAI-tech/Anuvaad"
 EMAIL = "praneeth@bpraneeth.com"
 AUTHOR = "BEDAPUDI PRANEETH"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 
 # What packages are required for this module to be executed?
 REQUIRED = ["transformers>=4.0.0rc1", "pydload", "torch>=1.5.0"]
