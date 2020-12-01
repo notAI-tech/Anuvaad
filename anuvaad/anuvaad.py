a47709d8815333c0875e909b55f4c1514f38b34a
Praneeth <praneethbedapudi@gmail.com>
2020-12-01 15:14:32 +0530

    use github releases for checkpoints
    Signed-off-by: Praneeth <praneethbedapudi@gmail.com>

--- anuvaad/anuvaad.py
+++ anuvaad/anuvaad.py
@@ -5,46 +5,64 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration
 
 MODEL_URLS = {
     "en-te": {
-        "pytorch_model.bin": "https://zenodo.org/record/4283506/files/en_te-pytorch_model.bin?download=1",
-        "config.json": "https://zenodo.org/record/4283506/files/en_te-config.json?download=1",
-        "special_tokens_map.json": "https://zenodo.org/record/4283506/files/en_te-special_tokens_map.json?download=1",
-        "spiece.model": "https://zenodo.org/record/4283506/files/en_te-spiece.model?download=1",
-        "tokenizer_config.json": "https://zenodo.org/record/4283506/files/en_te-tokenizer_config.json?download=1",
+        "pytorch_model.bin": [
+            "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_te-pytorch_model.bin.xaa",
+            "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_te-pytorch_model.bin.xab",
+        ],
+        "config.json": "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_te-config.json",
+        "special_tokens_map.json": "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_te-special_tokens_map.json",
+        "spiece.model": "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_te-spiece.model",
+        "tokenizer_config.json": "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_te-tokenizer_config.json",
     },
     "en-ta": {
-        "pytorch_model.bin": "https://zenodo.org/record/4283506/files/en_ta-pytorch_model.bin?download=1",
-        "config.json": "https://zenodo.org/record/4283506/files/en_ta-config.json?download=1",
-        "special_tokens_map.json": "https://zenodo.org/record/4283506/files/en_ta-special_tokens_map.json?download=1",
-        "spiece.model": "https://zenodo.org/record/4283506/files/en_ta-spiece.model?download=1",
-        "tokenizer_config.json": "https://zenodo.org/record/4283506/files/en_ta-tokenizer_config.json?download=1",
+        "pytorch_model.bin": [
+            "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_ta-pytorch_model.bin.xaa",
+            "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_ta-pytorch_model.bin.xab",
+        ],
+        "config.json": "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_ta-config.json",
+        "special_tokens_map.json": "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_ta-special_tokens_map.json",
+        "spiece.model": "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_ta-spiece.model",
+        "tokenizer_config.json": "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_ta-tokenizer_config.json",
     },
     "en-kn": {
-        "pytorch_model.bin": "https://zenodo.org/record/4283506/files/en_kn-pytorch_model.bin?download=1",
-        "config.json": "https://zenodo.org/record/4283506/files/en_kn-config.json?download=1",
-        "special_tokens_map.json": "https://zenodo.org/record/4283506/files/en_kn-special_tokens_map.json?download=1",
-        "spiece.model": "https://zenodo.org/record/4283506/files/en_kn-spiece.model?download=1",
-        "tokenizer_config.json": "https://zenodo.org/record/4283506/files/en_kn-tokenizer_config.json?download=1",
+        "pytorch_model.bin": [
+            "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_kn-pytorch_model.bin.xaa",
+            "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_kn-pytorch_model.bin.xab",
+        ],
+        "config.json": "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_kn-config.json",
+        "special_tokens_map.json": "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_kn-special_tokens_map.json",
+        "spiece.model": "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_kn-spiece.model",
+        "tokenizer_config.json": "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_kn-tokenizer_config.json",
     },
     "en-ml": {
-        "pytorch_model.bin": "https://zenodo.org/record/4283506/files/en_ml-pytorch_model.bin?download=1",
-        "config.json": "https://zenodo.org/record/4283506/files/en_ml-config.json?download=1",
-        "special_tokens_map.json": "https://zenodo.org/record/4283506/files/en_ml-special_tokens_map.json?download=1",
-        "spiece.model": "https://zenodo.org/record/4283506/files/en_ml-spiece.model?download=1",
-        "tokenizer_config.json": "https://zenodo.org/record/4283506/files/en_ml-tokenizer_config.json?download=1",
+        "pytorch_model.bin": [
+            "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_ml-pytorch_model.bin.xaa",
+            "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_ml-pytorch_model.bin.xab",
+        ],
+        "config.json": "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_ml-config.json",
+        "special_tokens_map.json": "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_ml-special_tokens_map.json",
+        "spiece.model": "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_ml-spiece.model",
+        "tokenizer_config.json": "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_ml-tokenizer_config.json",
     },
     "en-mr": {
-        "pytorch_model.bin": "https://zenodo.org/record/4283506/files/en_mr-pytorch_model.bin?download=1",
-        "config.json": "https://zenodo.org/record/4283506/files/en_mr-config.json?download=1",
-        "special_tokens_map.json": "https://zenodo.org/record/4283506/files/en_mr-special_tokens_map.json?download=1",
-        "spiece.model": "https://zenodo.org/record/4283506/files/en_mr-spiece.model?download=1",
-        "tokenizer_config.json": "https://zenodo.org/record/4283506/files/en_mr-tokenizer_config.json?download=1",
+        "pytorch_model.bin": [
+            "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_mr-pytorch_model.bin.xaa",
+            "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_mr-pytorch_model.bin.xab",
+        ],
+        "config.json": "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_mr-config.json",
+        "special_tokens_map.json": "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_mr-special_tokens_map.json",
+        "spiece.model": "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_mr-spiece.model",
+        "tokenizer_config.json": "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_mr-tokenizer_config.json",
     },
     "en-hi": {
-        "pytorch_model.bin": "https://zenodo.org/record/4283362/files/en_hi-pytorch_model.bin?download=1",
-        "config.json": "https://zenodo.org/record/4283362/files/en_hi-config.json?download=1",
-        "special_tokens_map.json": "https://zenodo.org/record/4283362/files/en_hi-special_tokens_map.json?download=1",
-        "spiece.model": "https://zenodo.org/record/4283362/files/en_hi-spiece.model?download=1",
-        "tokenizer_config.json": "https://zenodo.org/record/4283362/files/en_hi-tokenizer_config.json?download=1",
+        "pytorch_model.bin": [
+            "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_hi-pytorch_model.bin.xaa",
+            "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_hi-pytorch_model.bin.xab",
+        ],
+        "config.json": "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_hi-config.json",
+        "special_tokens_map.json": "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_hi-special_tokens_map.json",
+        "spiece.model": "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_hi-spiece.model",
+        "tokenizer_config.json": "https://github.com/notAI-tech/Anuvaad/releases/download/v1-checkpoints/en_hi-tokenizer_config.json",
     },
 }
 
@@ -127,7 +145,7 @@ class Anuvaad:
         ).input_ids
 
         if torch.cuda.is_available():
-            input_ids = input_ids.to('cuda')
+            input_ids = input_ids.to("cuda")
 
         output_ids = self.model.generate(
             input_ids, num_beams=beam_size, max_length=max_len
@@ -142,4 +160,3 @@ class Anuvaad:
             outputs = outputs[0]
 
         return outputs
-
